945a12837d472bdcb959011c01ccfa5c39d8a6d6
dheebinesh <dheebinesh472@gmail.com>
2026-01-05 16:26:02 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -19,11 +19,11 @@ class Bot(Client):
     def __init__(self):
         super().__init__(
             name="Bot",
-            api_hash=66c2f4b133d6422efee892228608573b,
+            api_hash="66c2f4b133d6422efee892228608573b",
             api_id=20400959,
             plugins={"root": "Plugins"},
             workers=1,
-            bot_token=8508740736:AAFDtMu1FpBYql9BddEBifkbkkFUEHKcYHA,
+            bot_token="8508740736:AAFDtMu1FpBYql9BddEBifkbkkFUEHKcYHA",
         )
    
     async def start(self, *args, **kwargs):
