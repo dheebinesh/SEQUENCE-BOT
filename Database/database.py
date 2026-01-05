49af5953557100bf81bafa10308867d3b28d4a38
dheebinesh <dheebinesh472@gmail.com>
2026-01-05 13:25:53 +0530

    database.py

--- Database/database.py
+++ Database/database.py
@@ -253,4 +253,4 @@ class Master:
 
 
 # Initialize
-Seishiro = Master(DB_URL, DB_NAME)
+Seishiro = Master(mongodb+srv://temek39194_db_user:<db_password>@cluster0.d87wtmf.mongodb.net/?appName=Cluster0, Rex_sequencebott)
